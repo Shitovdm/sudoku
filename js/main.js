76b36a72bef246751f947886e12084fae455ca70
ympo2oo2 <joj.lean@yandex.ru>
2018-02-13 20:30:41 +0300

    Добавлена функция вывода ошибки с полем для текста, добавлена проверка ячеек на совпадение в строках и столбцах.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-// Создание игрового поля
+// Создание поля
 $( document ).ready(function() {
 	$('<table>').appendTo('body');
+	$('body').append('<textarea id="OutputField">');
 	$('table').attr('id', 'workspace');
 	$('#workspace').append('<tr>');
+
 	for ( var i = 1; i <= 81; i++ ) {
 		if ( i % 9 == 1 ) {
 			$('<tr id = "row-'+(Math.ceil(i/9) )+'">').appendTo('#workspace');
@@ -14,8 +16,8 @@ $( document ).ready(function() {
 	// Строка и колонка.
 	for ( var i = 0; i < 9; i++ ) {
 		for ( var j = 0; j < 9; j++ ) {
-			$('#cell-' + ( ((i*9)+1)+j ) + ' input').attr('row',i+1);
-			$('#cell-' + ( ((j*9)+1)+i ) + ' input').attr('col',i+1);
+			$('#cell-' + ( ((i*9)+1)+j )).attr('row',i+1);
+			$('#cell-' + ( ((j*9)+1)+i )).attr('col',i+1);
 		}
 	}
 	// Номер квадрата.
@@ -23,14 +25,12 @@ $( document ).ready(function() {
 		for( var k = r; k < 8+r; k+=3, square++ ) {
 			for ( var i = 1; i <= 3; i++ ) {
 				for ( var j = -1; j <= 17; j+=9 ) {
-					$("#cell-" + ( i+k+j ) + " input").attr("square",square);
+					$('#cell-' + ( i+k+j )).attr('square',square);
 				}
 			}
 		}
 	}
-	
 });
-
 
 // Получение введенных данных
 function GetValues() {
@@ -40,6 +40,18 @@ function GetValues() {
 		Values.push($("#cell-" + i + " input").val());
 	}
 	console.log("Flag = " + CellsDataCheck(Values));
+}
+
+// Функция вывода ошибки
+function InputError(NumberOfIncorrectCell) {
+	$("#cell-" + NumberOfIncorrectCell + " input").addClass("incorrect-cell");
+	if ( $('#OutputField').val() ) {
+
+	} else {
+		$('#OutputField').val("Ошибка ввода данных!");
+	}
+
+	console.log("Error!");
 }
 
 // Проверка введенных данных ( целые числа от 0 до 9 )
@@ -52,15 +64,15 @@ function CellsDataCheck(CellsData) {
 					if ( (CellsData[i] > 0) && (CellsData[i] < 10) ) {
 						
 					} else {
-						$("#cell-" + (i+1) + " input").addClass("incorrect-cell");
+						InputError(i+1);
 						break;
 					}
 				} else {
-					$("#cell-" + (i+1) + " input").addClass("incorrect-cell");
+					InputError(i+1);
 					break;
 				}	
 			} else {
-				$("#cell-" + (i+1) + " input").addClass("incorrect-cell");
+				InputError(i+1);
 				break;
 			}
 		}
@@ -75,8 +87,47 @@ function CellsDataCheck(CellsData) {
 	return Flag;
 }
 
-// Проверка введенных данных на совпадение по строке, столбцу или в квадрате(3х3)
+// Проверка введенных данных на совпадение в строке, столбце или квадрате(3х3)
 function CellsDataCoincidence(CellsData) {
-	console.log("It still works");
+	var num = 0;
+	for ( var i = 0; i < 9; i++ ) { 
+		for ( var j = 1; j < 10; j++ ) { 
+			num++;
+			if ( +$('input[name = '+num+']').val() ) {	
+				for ( var k = 1; k < 10; k++ ) {
+					var x = i*9 + k;
+					var y = num + 9*k;
+					if ( num !== x ) {
+						if ( +$('input[name = '+ num +']').val() !== +$('input[name = '+ x +']').val() ) { // Совпадение в троке
+
+						} else {
+							InputError(num);
+							break;
+						}
+
+						if ( +$('input[name = '+ num +']').val() !== +$('input[name = '+ y +']').val() ) { // Совпадение в столбце
+
+						} else {
+							InputError(num);
+							InputError(y);
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// for ( var x = r = square = 1; x <= 55; x+=27,r = x ) {
+	// 	for( var k = r; k < 8+r; k+=3, square++ ) {
+	// 		for ( var i = 1; i <= 3; i++ ) {
+	// 			for ( var j = -1; j <= 17; j+=9 ) {
+	// 				$('#cell-' + ( i+k+j )).attr('square',square);
+	// 			}
+	// 		}
+	// 	}
+	// }
 
 }
+
+
